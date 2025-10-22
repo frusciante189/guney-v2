bd6b4304ac92cbb446bab9bea9d286bf0215bca3
frusciante189 <mcagdasbayirli@gmail.com>
2025-10-22 06:31:05 +0300

    hero banner added

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,40 @@
+import { ArrowUpRight } from "lucide-react";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="max-w-[1200px] mx-auto md:px-8 px-5 xl:pt-40 xl:pb-20 pt-28 md:pb-14 pb-10">
+      <div className="flex flex-col xl:gap-20 gap-16">
+        <div className="flex flex-col xl:gap-14 md:gap-11 gap-10">
+          <div className="flex flex-col md:gap-9 gap-8">
+            <div className="max-w-3xl mx-auto md:gap-6 gap-5">
+              <div className="bg-white border border-[#e5eaf0] rounded-full pl-3 py-1 pr-1 flex items-center">
+                <div className="size-1.5 bg-green-500 shadow-[0_0_0_2px_rgb(34_197_94/0.25)] rounded-full animate-pulse-strong" />
+                <p className="text-[#0f1115] text-xs py-1 pl-2.5 pr-3">
+                  Slots are available for October
+                </p>
+                <hr className="w-px h-3 bg-[#e2e7ee] sm:block hidden" />
+                <div className="sm:pl-3 py-0.5 pr-0.5 flex items-center gap-2">
+                  <p className="text-[#0f1115] text-xs sm:block hidden">
+                    Upcoming event: Munich 2026
+                  </p>
+                  <button className="bg-[#f3f5f6] cursor-pointer transition-colors size-6 rounded-full flex items-center justify-center">
+                    <ArrowUpRight color="#323745" size={12} />
+                  </button>
+                </div>
+              </div>
+              <div>2</div>
+              <div>3</div>
+            </div>
+            <div>2</div>
+          </div>
+          <div>a</div>
+        </div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
